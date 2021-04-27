5b7b407ab9fa8fde644e3cf076dcc9ea2298860c
MattHenao <2340@holbertonschool.com>
2021-04-27 14:50:18 -0500

    Last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,7 +4,7 @@ number = random.randint(-10000, 10000)
 n = number % 10
 
 if n > 5:
-    print('is {:d} and is greater than 5'.format(n)end="")
+    print('is {:d} and is greater than 5'.format(n), end="")
 
 elif n == 0:
     print('is {:d} and is zero'.format(n))
