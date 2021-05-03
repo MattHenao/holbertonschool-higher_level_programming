d17a72d175bc38d30b3b216600dd887bf07385de
MattHenao <2340@holbertonschool.com>
2021-05-03 10:40:27 -0500

    Prints the result of the addition of all arguments

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    result = 0
+    for numbers in range(len(sys.argv ) - 1):
+        result += int(sys.argv[numbers + 1])
+    print('{}'.format(result))
