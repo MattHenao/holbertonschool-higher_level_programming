5a2cb20f6100bce168380e1e43ae5566efc18b9c
MattHenao <2340@holbertonschool.com>
2021-05-06 17:56:32 -0500

    Prints a matrix of integers

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for count1 in range(len(matrix)):
         for count2 in range(len(matrix[count1])):
-            print('{:d}'.format(matrix[count1][count2]), end = '')
+            print('{:d}'.format(matrix[count1][count2]), end='')
             if count2 + 1 < len(matrix[count1]):
-                print(end = ' ')
+                print(end=' ')
         print()
