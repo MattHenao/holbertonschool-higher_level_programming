6cbeb884fb9da070185c649881b8d859ab88f81e
MattHenao <2340@holbertonschool.com>
2021-07-07 11:31:26 -0500

    Add private attribute size

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -6,8 +6,8 @@ class Square:
     """Build a class"""
     def __init__(self, size):
         """Initialize the attributes"""
-        self.__size = size
         if not isinstance(size, int):
             raise TypeError('size must be an integer')
         if size < 0:
             raise ValueError('size must be >= 0')
+        self.__size = size
