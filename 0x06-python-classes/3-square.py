dd044f4c2435db91884952fb0b2119e1e3e0a7d9
MattHenao <2340@holbertonschool.com>
2021-07-07 11:51:17 -0500

    Add public instance area

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""First class"""
+
+
+class Square:
+    """Build a class"""
+    def __init__(self, size=0):
+        """Initialize the attributes"""
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
+
+    def area(self):
+        return size ** 2
