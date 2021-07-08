531c5e69b709bf5db578fb5348594a81c00d23a6
MattHenao <2340@holbertonschool.com>
2021-07-08 11:34:28 -0500

    Setter

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""First class"""
+
+
+class Square:
+    """Build a class"""
+    def __init__(self, size=0):
+        """Initialize the attributes"""
+        self.__size = size
+    
+    def size(self):
+        """Return it"""
+        return self.__size
+
+    def size(self, value):
+        """The exception"""
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    def area(self):
+        """return the area"""
+        return self.__size ** 2
