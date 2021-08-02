f0af77f1f13c58501ad0385ca6a55295d5689760
MattHenao <2340@holbertonschool.com>
2021-08-02 17:47:29 -0500

    Args

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const arguments = process.argv;
+
+if (arguments.length <= 2){
+	console.log('No arguments');
+} else if (arguments.length === 3){
+	console.log('Argument found');
+} else{
+	console.log('Arguments found')
+}
