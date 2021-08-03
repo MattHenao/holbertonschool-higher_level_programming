3761fa464013e1dd229600f28e34cb7420de4b88
MattHenao <2340@holbertonschool.com>
2021-08-03 10:03:05 -0500

    Print "x" times

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const prueba = process.argv.slice(2);
+
+if (isNaN(parseInt(prueba[0]))){
+    console.log('Missing number of occurrences');
+} else{
+    for (let i = 0; i < prueba; i++) {
+        console.log('C is fun');
+    }
+}
