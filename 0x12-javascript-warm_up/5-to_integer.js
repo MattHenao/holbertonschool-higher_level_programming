c87d3adb85c81aec3d0aad62dd1d809237d78879
MattHenao <2340@holbertonschool.com>
2021-08-04 10:33:04 -0500

    Fix

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-const arguments = process.argv;
-const oneValue = arguments[2];
+const argument = process.argv;
+const oneValue = argument[2];
 
-if (!isNaN(oneValue)){
-	console.log('My number: ' + oneValue);
-} else{
-	console.log('Not a number');
+if (!isNaN(oneValue)) {
+  console.log('My number: ' + oneValue);
+} else {
+  console.log('Not a number');
 }
