1f9a0df9abf0eb17a5636e3a9f7633083d0c06aa
MattHenao <2340@holbertonschool.com>
2021-08-04 10:41:46 -0500

    Loops

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,10 +1,6 @@
 #!/usr/bin/node
-const phrases = [
-	'C is fun',
-	'Python is cool',
-	'JavaScript is amazing'
-];
-
-for (let value of phrases){
-	console.log(value);
+const phrases = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let count;
+for (count = 0; count < phrases.length; count++) {
+  console.log(phrases[count]);
 }
